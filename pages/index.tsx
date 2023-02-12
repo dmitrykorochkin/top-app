769c7d59969d80e6bd23cd3ae0f6c31bbe191546
Dmitry Korochkin <abramslost@gmail.com>
2023-02-12 17:14:36 +0300

    add tags h1-h3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import {Htag} from "../components";
 
-
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <div></div>
+    <div>
+    <Htag tag= "h1" > 44 </Htag>
+    </div>
   );
 }
