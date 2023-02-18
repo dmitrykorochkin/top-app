68dac45d23540fa6dea0e22114c9abbe9b20b870
Dmitry Korochkin <abramslost@gmail.com>
2023-02-18 15:25:50 +0300

    fix prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components";
 export default function Home(): JSX.Element {
   return (
     <div>
-      <h1>Текст1</h1>
-      <Button appearance='primary'>Button</Button>
-      <Button appearance='ghost'>Button</Button>
+      <h1>APP TOP</h1>
+      <Button appearance="primary">Button</Button>
+      <Button appearance="ghost">Button</Button>
     </div>
   );
 }
