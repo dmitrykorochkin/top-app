03627110f34f1a37961a5a019322c4c53e697d94
Dmitry Korochkin <abramslost@gmail.com>
2023-02-18 15:22:57 +0300

    add styles button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-
+import { Button } from "@/components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <h1>Текст1</h1>
-      <h2>Текст2</h2>
-      <h3>Текст3</h3>
+      <Button appearance='primary'>Button</Button>
+      <Button appearance='ghost'>Button</Button>
     </div>
   );
 }
