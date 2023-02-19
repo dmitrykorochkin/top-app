c51589bc1d2de8e9a2717b4fe7a7e6a9e507a035
Dmitry Korochkin <abramslost@gmail.com>
2023-02-19 11:30:50 +0300

    fix

--- components/P/P.props.ts
+++
@@ -1,7 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-import { ReactNode} from 'react';
-
-export interface PProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
-  size?: 's' | 'm' | 'l';
-  children: ReactNode
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
-import { P } from "../components";
-
 export default function Home(): JSX.Element {
   return (
     <div>
       <h1>APP TOP</h1>
       <button className="primary">Button</button>
       <button className="ghost">Button</button>
-      <P size="l">Большой</P>
-      <P>Средний</P>
-      <P size="s">Маленький</P>
+      <p className="fz-l">Большой</p>
+      <p className="fz-m">Средний</p>
+      <p className="fz-s">Маленький</p>
     </div>
   );
 }
