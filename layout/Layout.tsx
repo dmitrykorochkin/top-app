89f38adccfe058d3bfdac398cdfc2a843b55d84c
Dmitry Korochkin <abramslost@gmail.com>
2023-02-25 09:45:02 +0300

    add branch layout

---
+++ layout/Layout.tsx
@@ -0,0 +1,11 @@
+import cn from "classnames";
+import { LayoutProps } from "./Layout.props";
+import styles from "./Layout.module.css";
+
+
+export const Layout = ({children}: LayoutProps):JSX.Element => {
+  return (
+    <>
+    </>
+  )
+}
